214f3a3123c769723fa7e644225626f962c874fe
iye <iye@riotgames.com>
2019-03-31 16:36:01 +0900

    DogController에서 POST 방식의 /dogs endpoint를 노출합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
@@ -1,7 +1,14 @@
 package org.cnu.realcoding.weathercrawler.controller;
 
+import org.cnu.realcoding.weathercrawler.domain.Dog;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DogController {
+    @PostMapping("/dogs")
+    public void insertDog(@RequestBody Dog dog) {
+
+    }
 }
