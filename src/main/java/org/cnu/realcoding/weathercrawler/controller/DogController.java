e75dd864cf44d2ae916f297d9a12159a3a4b91d0
iye <iye@riotgames.com>
2019-03-31 17:00:51 +0900

    DogController에 모든 강아지를 반환하기위한 GET method를 정의합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
@@ -3,9 +3,12 @@ package org.cnu.realcoding.weathercrawler.controller;
 import org.cnu.realcoding.weathercrawler.domain.Dog;
 import org.cnu.realcoding.weathercrawler.service.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DogController {
@@ -15,5 +18,10 @@ public class DogController {
     @PostMapping("/dogs")
     public void insertDog(@RequestBody Dog dog) {
         dogService.insertDog(dog);
+    }
+
+    @GetMapping("/dogs")
+    public List<Dog> getAllDogs() {
+        return dogService.getAllDogs();
     }
 }
