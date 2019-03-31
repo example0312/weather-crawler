c4e732eee7e99a5f5b863e6fadf19afb6139b30c
iye <iye@riotgames.com>
2019-03-31 16:02:38 +0900

    실행 가능한 Spring boot 프로젝트를 생성합니다

---
+++ src/main/java/org/cnu/realcoding/weathercrawler/WeatherCrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.weathercrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherCrawlerApplication.class, args);
+	}
+
+}
