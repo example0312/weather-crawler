db9db6854e438e7520873f92c1c19bcf2a6a4c6c
iye <iye@riotgames.com>
2019-03-31 16:31:29 +0900

    DogService 클래스에 강아지 정보를 저장할 수 있는 List를 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/service/DogService.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/service/DogService.java
@@ -1,7 +1,12 @@
 package org.cnu.realcoding.weathercrawler.service;
 
+import org.cnu.realcoding.weathercrawler.domain.Dog;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DogService {
+    private List<Dog> dogList = new ArrayList<>();
 }
