c1cf6fb1c7eec6f31a5a827cc4d6e3662887d74b
iye <iye@riotgames.com>
2019-03-31 16:37:17 +0900

    DogController.insertDog() 에서 DogService 객체를 사용하여 강아지 정보를 삽입합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
@@ -1,14 +1,17 @@
 package org.cnu.realcoding.weathercrawler.controller;
 
 import org.cnu.realcoding.weathercrawler.domain.Dog;
+import org.cnu.realcoding.weathercrawler.service.DogService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DogController {
+    private DogService dogService;
+
     @PostMapping("/dogs")
     public void insertDog(@RequestBody Dog dog) {
-
+        dogService.insertDog(dog);
     }
 }
