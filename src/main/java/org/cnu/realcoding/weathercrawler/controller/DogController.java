3f8156c4f98a83016b9bcc704ecbafa395456f1d
iye <iye@riotgames.com>
2019-03-31 17:06:42 +0900

    DogController에 이름이 일치하는 강아지를 반환하기위한 PathVariable 방식의 GET method를 정의합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
@@ -3,10 +3,7 @@ package org.cnu.realcoding.weathercrawler.controller;
 import org.cnu.realcoding.weathercrawler.domain.Dog;
 import org.cnu.realcoding.weathercrawler.service.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,10 @@ public class DogController {
     @GetMapping("/dogs")
     public List<Dog> getAllDogs() {
         return dogService.getAllDogs();
+    }
+
+    @GetMapping("/dogs/{name}")
+    public Dog getDogByName(@PathVariable String name) {
+        return dogService.getDogsByName(name);
     }
 }
