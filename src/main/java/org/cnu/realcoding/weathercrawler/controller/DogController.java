06cea585a3af0de52504f2cce684828b21eb4964
iye <iye@riotgames.com>
2019-03-31 16:38:54 +0900

    Spring container가 DogController에서 사용하는 DogService 필드 변수에 객체를 injection 할 수 있도록 @Autowired 어노테이션을 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/controller/DogController.java
@@ -2,12 +2,14 @@ package org.cnu.realcoding.weathercrawler.controller;
 
 import org.cnu.realcoding.weathercrawler.domain.Dog;
 import org.cnu.realcoding.weathercrawler.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DogController {
+    @Autowired
     private DogService dogService;
 
     @PostMapping("/dogs")
