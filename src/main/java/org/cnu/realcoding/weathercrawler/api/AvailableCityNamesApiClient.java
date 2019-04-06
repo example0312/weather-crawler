e2b10c9d29ad9787d9ad3e045d8a44c9588b0205
iye <iye@riotgames.com>
2019-04-06 13:50:43 +0900

    RestTemplate을 사용하여 request를 날리고 응답받은 결과의 바디를 꺼냅니다

--- src/main/java/org/cnu/realcoding/weathercrawler/api/AvailableCityNamesApiClient.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/api/AvailableCityNamesApiClient.java
@@ -1,6 +1,7 @@
 package org.cnu.realcoding.weathercrawler.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,8 @@ public class AvailableCityNamesApiClient {
     private final String availableCitiesUri = "http://demo6468405.mockable.io/weather-crawlers/cities";
 
     public List<String> getAvilableCityNames() {
+        String[] responseBody = restTemplate.exchange(availableCitiesUri, HttpMethod.GET, null, String[].class)
+                .getBody();
         return null;
     }
 }
