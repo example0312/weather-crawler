ff99a5f026c37901fe446196e9b0ec5c5de11aa9
iye <iye@riotgames.com>
2019-04-06 14:59:47 +0900

    현재 날씨 응답에서 내가 사용하려는 것들만 도메인 클래스의 필드로 선언한다

--- src/main/java/org/cnu/realcoding/weathercrawler/domain/CurrentWeather.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/domain/CurrentWeather.java
@@ -4,4 +4,29 @@ import lombok.Data;
 
 @Data
 public class CurrentWeather {
+    private long id;
+    private String name;
+    private Main main;
+    private Wind wind;
+    private Others sys;
+
+    @Data
+    public static class Main {
+        private double temp;
+        private int pressure;
+        private int humidity;
+    }
+
+    @Data
+    public static class Wind {
+        private double speed;
+        private int deg;
+    }
+
+    @Data
+    public static class Others {
+        private String country;
+        private long sunrise;
+        private long sunset;
+    }
 }
