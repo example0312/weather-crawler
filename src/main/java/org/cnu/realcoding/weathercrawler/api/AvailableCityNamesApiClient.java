e94697f25d66b130989df8758c3b6d211c9fc737
iye <iye@riotgames.com>
2019-04-06 13:23:08 +0900

    Spring container에게 RestTemplate 객체를 주입(DI)해달라는 의미의 @Autowired 어노테이션을 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/api/AvailableCityNamesApiClient.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/api/AvailableCityNamesApiClient.java
@@ -1,9 +1,11 @@
 package org.cnu.realcoding.weathercrawler.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AvailableCityNamesApiClient {
+    @Autowired
     private RestTemplate restTemplate;
 }
