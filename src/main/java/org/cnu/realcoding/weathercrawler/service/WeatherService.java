974c8b5a3903d45ecca78f758816bf0d82739f34
iye <iye@riotgames.com>
2019-04-06 14:00:34 +0900

    AvailableCityNamesApiClient 필드를 선언합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/service/WeatherService.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/service/WeatherService.java
@@ -1,7 +1,9 @@
 package org.cnu.realcoding.weathercrawler.service;
 
+import org.cnu.realcoding.weathercrawler.api.AvailableCityNamesApiClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherService {
+    private AvailableCityNamesApiClient availableCityNamesApiClient;
 }
