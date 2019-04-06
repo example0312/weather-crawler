253a6aab2e0f6682b2668ffdcab31adfe127a69f
iye <iye@riotgames.com>
2019-04-06 14:01:37 +0900

    AvailableCityNamesApiClient 필드에 Spring container가 객체를 주입(DI)할 수 있도록 @Autowired를 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/service/WeatherService.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/service/WeatherService.java
@@ -1,9 +1,11 @@
 package org.cnu.realcoding.weathercrawler.service;
 
 import org.cnu.realcoding.weathercrawler.api.AvailableCityNamesApiClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherService {
+    @Autowired
     private AvailableCityNamesApiClient availableCityNamesApiClient;
 }
