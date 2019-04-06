c23f12fb4a533668aa0e8888427f66ebe3009aa3
iye <iye@riotgames.com>
2019-04-06 16:01:06 +0900

    FE에서 현재 날씨정보를 도시 이름으로 요청할 수 있도록 GET 방식의 endpoint를 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/controller/WeatherController.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/controller/WeatherController.java
@@ -1,8 +1,10 @@
 package org.cnu.realcoding.weathercrawler.controller;
 
+import org.cnu.realcoding.weathercrawler.domain.CurrentWeather;
 import org.cnu.realcoding.weathercrawler.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class WeatherController {
     @GetMapping("/available-cities")
     public List<String> getAvailableCityNames() {
         return weatherService.getAvailableCityNames();
+    }
+
+    @GetMapping("/current-weathers/by-city-name/{cityName}")
+    public CurrentWeather getCurrentWeatherByCityName(@PathVariable String cityName) {
+        return null;
     }
 }
