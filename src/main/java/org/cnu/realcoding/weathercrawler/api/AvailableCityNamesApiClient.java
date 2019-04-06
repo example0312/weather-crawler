0a804038d985e374c1b20f1390da17617621a275
iye <iye@riotgames.com>
2019-04-06 13:39:14 +0900

    AvailableCityNamesApiClient에 getAvilableCityNames 메소드를 정의합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/api/AvailableCityNamesApiClient.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/api/AvailableCityNamesApiClient.java
@@ -4,8 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 public class AvailableCityNamesApiClient {
     @Autowired
     private RestTemplate restTemplate;
+
+    public List<String> getAvilableCityNames() {
+        return null;
+    }
 }
