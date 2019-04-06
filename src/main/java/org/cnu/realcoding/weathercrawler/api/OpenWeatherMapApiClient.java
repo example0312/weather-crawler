43c81d6128663ef548b8c3a1ac0cd100ff10e67c
iye <iye@riotgames.com>
2019-04-06 14:36:38 +0900

    OpenWeatherMap에서 사용할 apiKey 값을 필드에 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/api/OpenWeatherMapApiClient.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/api/OpenWeatherMapApiClient.java
@@ -8,4 +8,6 @@ import org.springframework.web.client.RestTemplate;
 public class OpenWeatherMapApiClient {
     @Autowired
     private RestTemplate restTemplate;
+
+    private final String apiKey = "b59b2f6b4c62b33a007db6708713d1e6";
 }
