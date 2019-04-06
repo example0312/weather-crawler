ce5d1f37f2e3516fb103b22caf01374980c8b904
iye <iye@riotgames.com>
2019-04-06 13:27:28 +0900

    RestTemplate 객체를 반환하는 메소드를 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/config/HttpConfig.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/config/HttpConfig.java
@@ -1,7 +1,11 @@
 package org.cnu.realcoding.weathercrawler.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class HttpConfig {
+    public RestTemplate createRestTemplate() {
+        return new RestTemplate();
+    }
 }
