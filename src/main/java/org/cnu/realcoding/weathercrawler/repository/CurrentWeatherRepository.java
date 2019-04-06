618053e008af0f1c6be043caa4d14b63bbd12414
iye <iye@riotgames.com>
2019-04-06 15:43:42 +0900

    MongoTemplate 필드를 선언하고 @Autowired 을 추가한 후 MongoTemplate 이 구현된 디펜던시를 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/repository/CurrentWeatherRepository.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/repository/CurrentWeatherRepository.java
@@ -1,7 +1,11 @@
 package org.cnu.realcoding.weathercrawler.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CurrentWeatherRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 }
