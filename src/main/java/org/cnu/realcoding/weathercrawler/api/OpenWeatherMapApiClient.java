f5d365869e3aab07739779e640f084ee4c0bd208
iye <iye@riotgames.com>
2019-04-06 14:33:49 +0900

    RestTemplate 필드를 선언하고 @Autowired 를 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/api/OpenWeatherMapApiClient.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/api/OpenWeatherMapApiClient.java
@@ -1,7 +1,11 @@
 package org.cnu.realcoding.weathercrawler.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OpenWeatherMapApiClient {
+    @Autowired
+    private RestTemplate restTemplate;
 }
