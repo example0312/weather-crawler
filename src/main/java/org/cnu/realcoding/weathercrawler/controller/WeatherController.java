ce9fa4a84255dc4e761078792e990f9dc606058c
iye <iye@riotgames.com>
2019-04-06 14:10:17 +0900

    Controller 클래스에 @RequestMapping 을 추가하고 해당 어노테이션을 붙였을 때 일어나는 일들을 주석으로 추가합니다

--- src/main/java/org/cnu/realcoding/weathercrawler/controller/WeatherController.java
+++ src/main/java/org/cnu/realcoding/weathercrawler/controller/WeatherController.java
@@ -1,7 +1,14 @@
 package org.cnu.realcoding.weathercrawler.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 아래와 같이 Controller 클래스에 @RequestMapping 을 붙이면
+ * 아래 Controller 클래스에서 제공되는 Endpoints(API) 들의 기본 URI 가 다음과 같이 매핑된다.
+ * http://localhost:8080/weather-crawler
+ */
 @RestController
+@RequestMapping("/weather-crawler")
 public class WeatherController {
 }
